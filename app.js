4c585652114c90b4fc026e1a8dace7795b8b9ad2
quockhanh41 <115342969+quockhanh41@users.noreply.github.com>
2024-11-16 02:26:49 +0700

    initial folder

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const {engine} = require('express-handlebars'); // Import `engine` instead of `exphbs`
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Set up Handlebars view engine
+app.engine('hbs', engine({
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+}));
+app.set('view engine', 'hbs');
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define routes
+app.get('/', (req, res) => {
+    res.render('home', {layout: 'main'});
+});
+
+app.get('/index', (req, res) => {
+    res.render('home', {layout: 'main'});
+});
+
+app.get('/movies', (req, res) => {
+    res.render('movie-list', {layout: 'main'});
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
