10c15dd631d8dc5a6dc456d5b0dbea456f58895d
quockhanh41 <115342969+quockhanh41@users.noreply.github.com>
2024-11-17 01:48:14 +0700

    Mô tả commit của bạn

--- learn_rooting.js
+++ learn_rooting.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const PORT = 3000;
 
+// bai 1
 app.get('/', (req, res) => {
     res.send('Welcome to the Home Page');
 });
@@ -11,6 +12,13 @@ app.get('/contact', (req, res) => {
     res.send('Contac Us');
 })
 
+// bai 2
+app.get('/user/:userId', (req, res) => {
+    res.send('User ID: ' + req.params.userId);
+});
+app.get('/posts/:postId/comments/:commentId', (req, res) => {
+    res.send('Post ID: ' + req.params.postId );
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
