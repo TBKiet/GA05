9b7d8c3050c02fabc9748955ccfaf77381c78902
TBKiet <truongkiet771@gmail.com>
2024-11-17 01:43:34 +0700

    add test comment

--- test.js
+++ test.js
@@ -1,13 +1,15 @@
-require('./components/auth/config/db');
-const express = require('express');
+require("./components/auth/config/db");
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
-const userRouter = require('./components/auth/api/user');
+const userRouter = require("./components/auth/api/user");
 
 app.use(express.json());
-app.use('/user', userRouter);
+app.use("/user", userRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+//asdfsdfasdfasdjfh asdjkfh sadjkh sdhf kjsdh skdjh kj
