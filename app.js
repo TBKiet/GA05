38b8c863fee9dc19fc7c77562c94e3df5f289e62
TBKiet <truongkiet771@gmail.com>
2024-11-17 06:59:15 +0700

    save images to database and show to web

--- app.js
+++ app.js
@@ -1,52 +1,148 @@
-const express = require('express');
-const {engine} = require('express-handlebars'); // Import `engine` instead of `exphbs`
-const path = require('path');
+const express = require("express");
+const { engine } = require("express-handlebars"); // Import `engine` instead of `exphbs`
+const path = require("path");
+const cloudinary = require("./cloud");
+
+const movies = [
+  {
+    title: "Thiên Đường Quả Báo",
+    imageUrl:
+      "https://res.cloudinary.com/djupm4v0l/image/upload/coeedkw4jxpyo9ojvlo6?_a=BAMCkGOa0",
+    rating: 8.9,
+    ageLimit: "T18",
+  },
+  {
+    title: "Venom: Kèo Cuối",
+    imageUrl:
+      "https://res.cloudinary.com/djupm4v0l/image/upload/ifhpolrj9xeorwmbj8eo?_a=BAMCkGOa0",
+    rating: 9.1,
+    ageLimit: "T13",
+  },
+  {
+    title: "Cô Dâu Hào Môn",
+    imageUrl:
+      "https://res.cloudinary.com/djupm4v0l/image/upload/film3_jo9cz2?_a=BAMCkGOa0",
+    rating: 7.5,
+    ageLimit: "T18",
+  },
+  {
+    title: "Ngày Xưa Có Một Chuyện Tình",
+    imageUrl:
+      "https://res.cloudinary.com/djupm4v0l/image/upload/film1_skupam?_a=BAMCkGOa0",
+    rating: 8.6,
+    ageLimit: "T16",
+  },
+  {
+    title: "Học Viện Anh Hùng: You're Next",
+    imageUrl:
+      "https://res.cloudinary.com/djupm4v0l/image/upload/up3_dvpucn?_a=BAMCkGOa0",
+    rating: 9.5,
+    ageLimit: "K",
+  },
+  {
+    title: "Godzilla - 1.0",
+    imageUrl:
+      "https://res.cloudinary.com/djupm4v0l/image/upload/gmo_niafh7?_a=BAMCkGOa0",
+    rating: 8.1,
+    ageLimit: "T13",
+  },
+  {
+    title: "Akira",
+    imageUrl:
+      "https://res.cloudinary.com/djupm4v0l/image/upload/akira_xfqj1o?_a=BAMCkGOa0",
+    rating: 10,
+    ageLimit: "T16",
+  },
+  {
+    title: "Đi Karaoke Đi!",
+    imageUrl:
+      "https://res.cloudinary.com/djupm4v0l/image/upload/dkd_u8yz37?_a=BAMCkGOa0",
+    rating: 10,
+    ageLimit: "T13",
+  },
+  {
+    title: "Tình Ta Đẹp Tựa Đóa Hoa",
+    imageUrl:
+      "https://res.cloudinary.com/djupm4v0l/image/upload/ttdtdh_wquvpf?_a=BAMCkGOa0",
+    rating: 8.9,
+    ageLimit: "T16",
+  },
+  {
+    title: "Những Kẻ Bên Lề",
+    imageUrl:
+      "https://res.cloudinary.com/djupm4v0l/image/upload/bap_gs52cw?_a=BAMCkGOa0",
+    rating: 6,
+    ageLimit: "T16",
+  },
+  {
+    title: "Cái Ác Không Tồn Tại",
+    imageUrl:
+      "https://res.cloudinary.com/djupm4v0l/image/upload/edne_btpv0g?_a=BAMCkGOa0",
+    rating: 8,
+    ageLimit: "K",
+  },
+  {
+    title: "Mẹ À?!",
+    imageUrl:
+      "https://res.cloudinary.com/djupm4v0l/image/upload/mity_nigfdk?_a=BAMCkGOa0",
+    rating: 7,
+    ageLimit: "T13",
+  },
+];
 
 const app = express();
 const PORT = 3000;
 
 // Set up Handlebars view engine
-app.engine('hbs', engine({
-    extname: '.hbs',
-    layoutsDir: path.join(__dirname, 'views', 'layouts'),
-    partialsDir: path.join(__dirname, 'views', 'partials'),
-}));
-app.set('view engine', 'hbs');
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+    layoutsDir: path.join(__dirname, "views", "layouts"),
+    partialsDir: path.join(__dirname, "views", "partials"),
+  })
+);
+app.set("view engine", "hbs");
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Define routes
-app.get('/', (req, res) => {
-    res.render('home', {layout: 'main'});
+app.get("/", (req, res) => {
+  res.render("home", { layout: "main" });
 });
 
-app.get('/index', (req, res) => {
-    res.render('home', {layout: 'main'});
+app.get("/index", (req, res) => {
+  res.render("home", { layout: "main" });
 });
 
-app.get('/movie-details', (req, res) => {
-    res.render('movie-details', {layout: 'main'});
+app.get("/movie-details", (req, res) => {
+  res.render("movie-details", { layout: "main"});
 });
 
-app.get('/movies', (req, res) => {
-    res.render('movie-list', {layout: 'main'});
+// app.get('/movies', (req, res) => {
+//     res.render('movie-list', {layout: 'main'});
+// });
+
+app.get("/movies", (req, res) => {
+  // Pass the movie data to the Handlebars template
+  res.render("movie-list", { layout: "main", movies });
 });
 
-app.get('/about', (req, res) => {
-    res.render('about', {layout: 'main'});
+app.get("/about", (req, res) => {
+  res.render("about", { layout: "main" });
 });
 
-app.get('/contact', (req, res) => {
-    res.render('contact', {layout: 'main'});
+app.get("/contact", (req, res) => {
+  res.render("contact", { layout: "main" });
 });
-app.get('/register', (req, res) => {
-    res.render('register', {layout: 'main'});
+app.get("/register", (req, res) => {
+  res.render("register", { layout: "main" });
 });
-app.get('/login', (req, res) => {
-    res.render('login', {layout: 'main'});
+app.get("/login", (req, res) => {
+  res.render("login", { layout: "main" });
 });
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ movie-detail.js
@@ -0,0 +1,17 @@
+const details = {
+    title: "Thiên Đường Quả Báo",
+    trailerUrl: "https://www.youtube.com/embed/2-oT85FKYsQ?si=F2dJH8CIcA2RkHWt",
+    imageUrl1: "https://res.cloudinary.com/djupm4v0l/image/upload/coeedkw4jxpyo9ojvlo6?_a=BAMCkGOa0",
+    duration: "131 phút",
+    releaseDate: "30/10/2024",
+    rating: 8.9,
+    votes: 49,
+    country: "Thái Lan",
+    producers: ["GDH 559", "Jai Studios"],
+    genres: ["Tâm Lý", "Giật Gân"],
+    directors: ["Boss Kuno"],
+    actors: ["Jeff Satur", "Engfa Waraha"],
+    synopsis: `Thongkam (Jeff Satur) và Sek (Pongsakorn) làm lụng vất vả, cày ngày cày đêm để xây dựng một mái ấm. Sau bao nỗ lực, cuối cùng họ cũng có quyền sở hữu trang trại sầu riêng ở Mae Hong Son. Tuy nhiên, bi kịch bỗng ập tới khi Sek đột ngột qua đời trong một vụ tai nạn vào thời điểm họ chưa kịp đăng ký kết hôn. Chính điều này đã khiến Thongkam không có quyền sở hữu tài sản mà 2 người đã cùng gây dựng. Ngôi nhà và đất đai bao gồm vườn sầu riêng đương nhiên thuộc về mẹ của Sek, người sẽ chuyển đến sống cùng con gái nuôi Mo (Engfa) và người làm vườn Jingna. Thongkam giờ phải đấu tranh để giành lại thành quả của tình yêu và công sức lao động của mình. Liệu giữa Thongkam và Mo, ai sẽ giành được quyền thừa kế, trở thành chủ vựa sầu riêng?`
+};
+
+module.exports = details;
