6805684ed875d355375234383902b3bea62a8dca
TBKiet <truongkiet771@gmail.com>
2024-11-24 22:17:47 +0700

    Fix Filter 3

--- components/movies/movies.controller.js
+++ components/movies/movies.controller.js
@@ -1,31 +1,9 @@
 const { getMovieById } = require("../movies/service");
 const { getMovieLists } = require("../users/service");
 
-function filterMoviesByGenre(movies, selectedGenre) {
-  console.log("GENRE:   ", selectedGenre);
-  // Ensure the selected genre is not empty
-  if (!selectedGenre) return movies;
-
-  // Filter movies where the genre string includes the selected genre
-  return movies.filter(movie => {
-    if (movie.genre && movie.genre.trim()) {
-      // Split the genre string if it's a comma-separated list
-      const movieGenres = movie.genre.split(',').map(g => g.trim());
-      return movieGenres.some(g => g.toLowerCase() === selectedGenre.trim().toLowerCase());
-    }
-    return false;
-  });
-}
-
 exports.renderMovieList = async (req, res) => {
   try {
     const { movieList } = await getMovieLists();
-
-    // Get selected genre from query parameters
-    const selectedGenre = req.query.genre;
-
-    // Filter movies based on the selected genre
-    const filteredMovies = filterMoviesByGenre(movieList, selectedGenre);
 
     // Ensure genres are unique and sorted
     const genres = [...new Set(movieList.flatMap(movie => movie.genre.split(',').map(g => g.trim())))].sort();
@@ -35,7 +13,7 @@ exports.renderMovieList = async (req, res) => {
 
     res.render("movie-list", {
       layout: "main",
-      movies: filteredMovies, // Use filtered movies
+      movies: movieList, // Use filtered movies
       genres,
       ages,
       ratings,
