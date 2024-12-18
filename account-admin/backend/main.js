bb7f23b073f8e212211ddafcee7ea360e51e7b10
HoangLinh-FITUS <thlinh22@clc.fitus.edu.vn>
2024-12-18 18:40:35 +0700

    create api

--- account-admin/backend/main.js
+++ account-admin/backend/main.js
@@ -1,28 +1,66 @@
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const cors = require('cors'); // Import thư viện CORS
 
-const url = 'mongodb+srv://quockhanh41:9hNKsR08vJlurpnO@cluster0.qrnq7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+// URL kết nối tới MongoDB UserDB
+const url = "mongodb+srv://quockhanh41:9hNKsR08vJlurpnO@cluster0.qrnq7.mongodb.net/UserDB?retryWrites=true&w=majority&appName=Cluster0";
 
 const PORT = 8080;
-const HOSTNAME = "localhost"
+const HOSTNAME = "localhost";
 
-app = express();
+const app = express();
+app.use(cors());
 
-mongoose.connect(url);
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    role: String
-  });
-
-  const User = mongoose.model('User', userSchema);
-  const users = User.find();  
-  console.log(users)
-
-app.get("/api/users", (req, res) => {
-    res.json(users)
+mongoose.connect(url)
+.then(() => {
+    console.log("Kết nối MongoDB thành công!");
+  })
+.catch((err) => {
+    console.error("Lỗi kết nối MongoDB:", err);
 });
 
-app.listen(PORT, (res, req) => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-})
+// Định nghĩa Schema cho collection users
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  role: String,
+  data: Date,
+  phone: String,
+});
+
+const userDB = mongoose.connection.useDb("UserDB");
+const User = userDB.model("users", userSchema);
+
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find(); 
+    res.json(users); 
+  } catch (err) {
+    console.error("Lỗi khi lấy dữ liệu người dùng:", err);
+    res.status(500).json({ error: "Lỗi server" });
+  }
+});
+
+const moviesSchema = new mongoose.Schema({
+    name_vn: String,
+    name_en: String,
+});
+
+const movieDB = mongoose.connection.useDb("MovieDB");
+const movies = movieDB.model("movies", moviesSchema);
+
+app.get("/api/movies", async (req, res) => {
+    try {
+      const movie = await movies.find(); 
+      res.json(movie); 
+    } catch (err) {
+      console.error("Lỗi khi lấy dữ liệu phim:", err);
+      res.status(500).json({ error: "Lỗi server" });
+    }
+});
+  
+
+// Khởi chạy server
+app.listen(PORT, () => {
+  console.log(`Server đang chạy tại api của http://${HOSTNAME}:${PORT}`);
+});
---
+++ account-admin/frontend/account-management/script.js
@@ -0,0 +1,54 @@
+API_USER = "http:/localhost:8080/api/users"
+
+fetch(API_USER) 
+.then((res) => res.json())
+.then((data) => {
+    let res = `
+    <tr>
+        <th>Actions</th>
+        <th>Name</th>
+        <th>Role</th>
+        <th>Date</th>
+        <th>Phone</th>
+        <th>Email</th>
+    </tr>
+    `
+
+    data.forEach((user)=>{
+        const username = user.username
+        const email = user.email
+        const role = user.role
+        
+        let phone = null
+        if ('phone' in user) phone = user.phone
+        
+        let date = null;
+        if ("activationExpires" in user) date = user.activationExpires
+
+        date = new Date(date)
+        date = date.toLocaleString('vi-VN', {
+            year: 'numeric', 
+            month: 'numeric', 
+            day: 'numeric'
+        })
+        const isActive = user.isActive
+
+        res += `
+            <tr>
+                <td>
+                    <input type="checkbox">
+                    <button class="edit-btn">Edit</button>
+                    <button class="delete-btn">Delete</button>
+                </td>
+                <td>${username}</td>
+                <td>${role}</td>
+                <td>${date}</td>
+                <td>${phone}</td>
+                <td>${email}</td>
+            </tr>
+        `
+    })
+
+    const userList = document.getElementById("user-db")
+    userList.innerHTML = res
+})
